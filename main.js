fc527f8d86c61ff8eb8e606f09ada889c662c446
BCAvZ <Bartvanzanen@hotmail.com>
2022-07-06 20:59:29 +0200

    huiswerk  beste poging

--- main.js
+++ main.js
@@ -161,3 +161,198 @@ const inventory = [
     sold: 8,
   },
 ];
+
+// opdracht 1.1
+// const tvTypes = inventory.map((tvType) => {
+//   return tvType.type;
+// })
+//
+// console.log(tvTypes);
+
+
+// opdracht 1.2
+
+// const soldout = inventory.filter ((tv) => {
+//   return tv.originalStock - tv.sold === 0;
+// })
+//
+// console.log(soldout)
+
+// opdracht 1.3
+// const ambiLightCheck = inventory.filter ((ambiTv) => {
+//   return ambiTv.options.ambiLight === true
+// })
+//
+// console.log(ambiLightCheck)
+
+// opdracht 1.4
+// const salesPrice = inventory.sort ((a,b) => {
+//   return a.price - b.price;
+// })
+//
+// console.log(salesPrice);
+
+
+// Opdracht 2 - Elementen in de DOM plaatsen
+// Tip: wanneer we meerdere waardes uit een array willen terugbrengen tot één getal of één string, gebruik je hier gewoon een oude vertrouwde for-loop voor!
+//
+// Opdracht 2a: Hoeveel tv's zijn er al verkocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+
+// let outcome = 0;
+//
+// for (let i = 0; i < inventory.length; i++) {
+//  outcome += inventory[i].sold;
+// }
+// console.log(outcome);
+
+
+
+// let soldTv2 = 0;
+//
+// inventory.map ((soldTv) => {
+//   return (soldTv2 += soldTv.sold);
+// })
+//
+// console.log(soldTv2);
+
+// Opdracht 2b: Zorg ervoor dat dit aantal in het groen wordt weergegeven op de pagina.
+// const element2 = document.getElementById("soldTv");
+// element2.textContent = `Amount of sold TV's: ${soldTv2}`;
+
+
+
+//     Opdracht 2c: Hoeveel tv's heeft Tech It Easy ingekocht? Schrijf een script dat dit berekent. Log de uitkomst in de console.
+// let purchasedInventory2 = 0;
+//
+// inventory.map ((purchasedinventory) => {
+//   return (purchasedInventory2 += purchasedinventory.originalStock);
+// })
+//
+// console.log(purchasedInventory2);
+
+// Opdracht 2d: Zorg ervoor dat dit aantal in het blauw wordt weergegeven op de pagina.
+// const element3 = document.getElementById("purchasedInventory");
+// element3.textContent = `Amount of purchased TV's: ${purchasedInventory2}`;
+
+//     Opdracht 2e: Geef in het rood weer hoeveel tv's er nog verkocht moeten worden.
+
+// let remainingInventory2 = 0;
+//
+// inventory.map ((remainingInventory) => {
+//   return remainingInventory2 += (remainingInventory.originalStock - remainingInventory.sold
+// )})
+//
+// console.log(remainingInventory2);
+//
+// const element4 = document.getElementById("toBeSoldTv");
+// element4.textContent = `Amount of TV's yet to be sold: ${remainingInventory2}`;
+
+// Opdracht 3a: Gebruik een array-methode om alle tv merken (zoals Philips, NIKKEI, etc.) in een lijst op de pagina weer te geven. Zorg ervoor dat dit ook zou werken als we 200 tv's in onze array zouden hebben staan. Dat er dubbele namen in zitten, is niet erg.
+
+// const productNames = inventory.map ((productName) => {
+//   return productName.brand;
+// })
+//
+// console.log(productNames);
+
+// Opdracht 3b: Schrijf de code uit 3a om naar een functie die een array met tv-objecten verwacht. Het is handig om onze scripts als functies op te zetten, zodat we ze gemakkelijk kunnen hergebruiken. Tip: vergeet deze functie -declaratie niet aan te roepen!
+
+// let outcome6 = "";
+//
+// const productnames3 = (input) => {
+//   for (let i = 0; i < inventory.length; i++) {
+//     outcome6 += input;
+//   } return outcome6;
+// }
+//
+// const element6 = document.getElementById('banaan');
+// element6.textContent = `The tv brand names are: ${productnames3(inventory[i].brand)}`;
+
+
+
+// const productNames2 = inventory.map ((productName) => {
+//   return productName.brand;
+// })
+//
+// const element6 = document.getElementById('banaan');
+// element6.textContent = `The tv brand names are: ${productNames2}`;
+
+
+// Opdracht 4a: Maak een herbruikbare functie die een string genereert voor de naam van één tv en deze teruggeeft in het format [merk] [type] - [naam] zoals Philips 43PUS6504/12 - 4K TV of NIKKEI NH3216SMART - HD smart TV.
+
+
+const productNames4 = (input2) => {
+  let outcome7;
+  for (let i = 0; i < inventory.length; i++) {
+
+    const tvBrand = inventory[input2].brand
+    const tvType = inventory[input2].type
+    const tvName = inventory[input2].name
+
+    outcome7 = tvBrand + ' ' + tvType + ' - ' + tvName;
+  } return outcome7
+}
+
+const element7 = document.getElementById('list');
+element7.textContent = ` ${productNames4(1)}`;
+
+// Opdracht 4b: Maak een herbruikbare functie die de prijs van één tv als parameter verwacht (zoals 379 of 159) teruggeeft in het format €379,- of €159,-.
+//
+
+let outcome8 = '';
+
+const productNames5 = (input3) => {
+  let outcome8;
+  for (let i = 0; i < inventory.length; i++) {
+    outcome8 = inventory[input3].price;
+  } return outcome8;
+}
+
+const element8 = document.getElementById('lis');
+element8.textContent = `€${productNames5(2)},-`;
+
+// Opdracht 4c: Maak een herbruikbare functie die een string genereert voor alle beschikbare schermgroottes van één tv. De functie geeft dit terug in het format [schermgrootte] inches ([schermgrootte omgerekend]cm) | [schermgrootte] inches ([schermgrootte omgerekend]cm) etc. Als een tv maar één schermgrootte heeft ([32]) wordt de output 32 inch (81 cm). Wanneer een tv vier schermgroottes heeft ([43, 50, 55, 58]) wordt de output 43 inch (109 cm) | 50 inch (127 cm) | 58 inch (147 cm). Let op: om één string te genereren uit een array van schermgroottes zul je een for-loop voor moeten gebruiken.
+//
+
+function availableScreenSizes ( input8 ) {
+  let size = "";
+  for (let i = 0; i < inventory.length; i++) {
+      const sizeInInch = inventory[input8].availableSizes;
+      const sizeInCm = inventory[input8].availableSizes * 2.54;
+
+      size += sizeInInch + ' inch ('+ sizeInCm + ' cm). | ';
+  } return size;
+}
+
+// ik snap er niks van dit zou moeten werken
+
+const element9 = document.getElementById("li");
+element9.textContent = availableScreenSizes(0);
+
+// function availableScreenSizes2 ( input ){
+//   let size = "";
+//   for (let i = 0; i < input.length; i++) {
+//     const sizeInInch = input[i];
+//     const sizeInCM = input[i] * 2.54;
+//     //const stringScreenSize = `Screen sizes are: ${sizeInInch} inch | In centimeters that is: ${sizeInCM} cm`;
+//     size = size + sizeInInch + ` inch | In centimeters that is: ${sizeInCM} cm \n`
+//   }   return size;
+// }
+// const screenSizesTv = document.getElementById("l");
+// screenSizesTv.textContent = availableScreenSizes2(inventory[3].availableSizes);
+
+
+//     Opdracht 4d: Schrijf een script die de informatie van de Philips 43PUS6504/12 tv weergeeft op de pagina zoals onderstaand voorbeeld. Gebruik de functies die je hebt gemaakt in opdracht 4a, 4b en 4c.
+
+// let displayTotalInfo = "";
+//
+// const totalInfo = (parameter) => {
+//   return displayTotalInfo = availableScreenSizes(parameter) + productNames5(parameter) + productNames4(parameter)
+// }
+//
+// const element10 = document.getElementById("l");
+// element10.textContent = totalInfo(0);
+
+
+
+// Opdracht 4e: Maak een herbruikbare functie die de informatie van alle tv's weergeeft op de pagina. Gebruik hiervoor de map-methode in combinatie met de functies die je hebt gemaakt in opdracht 4a, 4b en 4c.
